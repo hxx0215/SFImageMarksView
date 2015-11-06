841e58605fa956ee9372f2cc38aae39656e2bdfb
hxx0215 <hxx0215@126.com>
2015-11-06 21:46:08 +0800

    init

---
+++ SFImageMarksView/SFImageMarksView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SFImageMarksView
+//
+//  Created by shadowPriest on 15/11/6.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
