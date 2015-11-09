c45cdb2f3344325b1c0395c437076afa5b1b2bb5
hxx0215 <hxx0215@126.com>
2015-11-09 21:03:46 +0800

    drawlineView

---
+++ SFImageMarksView/SFImageMarksView/DrawLine.swift
@@ -0,0 +1,31 @@
+//
+//  DrawLine.swift
+//  SFImageMarksView
+//
+//  Created by shadowPriest on 15/11/9.
+//
+//
+
+import UIKit
+
+class DrawLine: UIView {
+
+    let path = UIBezierPath()
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+    }
+}
