284e55407cfa1e2151025a97da7d7c2a3ffa31a8
hxx0215 <hxx0215@126.com>
2015-11-10 21:31:49 +0800

    iOS9 coalescedTouchesForTouch test

--- SFImageMarksView/SFImageMarksView/DrawLine.swift
+++ SFImageMarksView/SFImageMarksView/DrawLine.swift
@@ -8,24 +8,80 @@
 
 import UIKit
 
+enum DrawLinePosition {
+    case Up
+    case Down
+}
+
+protocol DrawLineDelegate : class{
+    func shouldDraw(view: DrawLine)
+}
+
 class DrawLine: UIView {
 
-    let path = UIBezierPath()
+    var path = UIBezierPath()
+    var path2 = UIBezierPath()
+    weak var delegate:DrawLineDelegate?
+    
+    lazy var position:DrawLinePosition = {
+        return .Up
+    }()
     // Only override drawRect: if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
     override func drawRect(rect: CGRect) {
         // Drawing code
+        let context = UIGraphicsGetCurrentContext()
+        CGContextSetLineCap(context, .Round)
+        CGContextSetLineJoin(context, .Round)
+        CGContextSetLineWidth(context, 2.0)
+        let color = UIColor.redColor()
+        CGContextSetStrokeColorWithColor(context, color.CGColor)
+
+        if position == .Up{
+            CGContextAddPath(context, self.path.CGPath)
+        }else{
+            CGContextAddPath(context, self.path2.CGPath)
+        }
+        CGContextStrokePath(context)
     }
 
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        
+        if let point = touches.first?.locationInView(self) {
+            self.path.moveToPoint(point)
+            self.path2.moveToPoint(point)
+        }
     }
     
     override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        
+        guard let e = event else{
+            return;
+        }
+        guard let touch = touches.first else{
+            return;
+        }
+        if let coalescedTouch = e.coalescedTouchesForTouch(touch){
+            if let point = coalescedTouch.first?.locationInView(self){
+                self.path.addLineToPoint(point)
+            }
+        }
+        if let point = touches.first?.locationInView(self){
+            self.path2.addLineToPoint(point)
+        }
+        if let del = self.delegate{
+            del.shouldDraw(self)
+        }
     }
     
     override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        
+        if let point = touches.first?.locationInView(self) {
+            self.path.addLineToPoint(point)
+            self.path2.addLineToPoint(point)
+        }
+        self.setNeedsDisplay()
     }
 }
--- SFImageMarksView/SFImageMarksView/ViewController.swift
+++ SFImageMarksView/SFImageMarksView/ViewController.swift
@@ -8,14 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,DrawLineDelegate {
 
+    @IBOutlet weak var lineView1: DrawLine!
+    @IBOutlet weak var lineView2: DrawLine!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        lineView1.position = .Up
+        lineView2.position = .Down
+        lineView1.delegate = self
+        lineView2.delegate = self
     }
 
-
+    func shouldDraw(view: DrawLine) {
+        if view == lineView1{
+            lineView2.path2 = lineView1.path2
+            lineView2.path = lineView1.path
+        }else{
+            lineView1.path = lineView2.path
+            lineView1.path2 = lineView2.path2
+        }
+        lineView1.setNeedsDisplay()
+        lineView2.setNeedsDisplay()
+    }
 
 }
 
