01c63f1d0a3cbc46e4931816354fb60b4aa63a64
hxx0215 <hxx0215@126.com>
2015-11-11 20:13:10 +0800

    add a smooth draw

---
+++ SFImageMarksView/SFImageMarksView/SmoothDraw.swift
@@ -0,0 +1,21 @@
+//
+//  SmoothDraw.swift
+//  SFImageMarksView
+//
+//  Created by shadowPriest on 15/11/11.
+//
+//
+
+import UIKit
+
+class SmoothDraw: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
