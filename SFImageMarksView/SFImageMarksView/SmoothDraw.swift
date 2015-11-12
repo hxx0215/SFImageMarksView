88295975c5d89aa1f59ad01ba63e4ae2983087aa
hxx0215 <hxx0215@126.com>
2015-11-12 20:20:19 +0800

    no message

--- SFImageMarksView/SFImageMarksView/SmoothDraw.swift
+++ SFImageMarksView/SFImageMarksView/SmoothDraw.swift
@@ -10,12 +10,15 @@ import UIKit
 
 class SmoothDraw: UIView {
 
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
     }
-    */
 
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+    }
 }
