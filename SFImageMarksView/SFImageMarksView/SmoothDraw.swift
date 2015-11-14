85d7139540269f63f1366e2c666db0af47273a01
hxx0215 <hxx0215@126.com>
2015-11-14 21:53:21 +0800

    no message

--- SFImageMarksView/SFImageMarksView/SmoothDraw.swift
+++ SFImageMarksView/SFImageMarksView/SmoothDraw.swift
@@ -11,8 +11,15 @@ import UIKit
 class SmoothDraw: UIView {
 
     let path = UIBezierPath()
+    var previousPoint1 = CGPointZero
+    var previousPoint2 = CGPointZero
+    var currentPoint = CGPointZero
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        
+        guard let touch = touches.first else{
+            return;
+        }
+        self.previousPoint1 = touch.previousLocationInView(self)
+        self.currentPoint = touch.locationInView(self)
     }
 
     override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
